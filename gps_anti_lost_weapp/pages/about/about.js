1bd98c17a4540b0d1e4ae86be88957b26ac95a9c
yi985432 <yi985432@gmail.com>
2026-07-14 21:13:53 +0800

    feat: 关于页面添加仓库地址

--- gps_anti_lost_weapp/pages/about/about.js
+++ gps_anti_lost_weapp/pages/about/about.js
@@ -4,5 +4,15 @@ Page({
     if (typeof this.getTabBar === 'function' && this.getTabBar()) {
       this.getTabBar().setData({ selected: 2 })
     }
+  },
+
+  // 复制仓库地址
+  copyRepo() {
+    wx.setClipboardData({
+      data: 'https://github.com/yi985432/Pet-Tracker',
+      success: () => {
+        wx.showToast({ title: '仓库地址已复制', icon: 'success' })
+      }
+    })
   }
 })
